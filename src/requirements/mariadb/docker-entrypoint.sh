3355814a047c77b5b33fe847fab8d5200c8159a2
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-05 14:16:09 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -5,17 +5,17 @@ set -e
 /usr/bin/mysqld --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM	mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+-- Add the following line to grant access to the WordPress container:
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'wordpress.inception_backend';
 FLUSH PRIVILEGES;
 EOF
-
-# fi
 
 exec "$@"
