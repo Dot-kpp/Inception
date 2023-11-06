580330e9d9b326aad8b1540c18d3052c6eeec6be
Dot-kpp <79651465+Dot-kpp@users.noreply.github.com>
2023-11-06 14:19:36 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -88,7 +88,8 @@
 mysqld_safe &
 
 # Wait for MariaDB server to start (max 30 seconds)
-sleep 10
+echo "Waiting for MariaDB server to accept connections"
+sleep 6
 timeout=30
 while ! mysqladmin ping &>/dev/null
 do
@@ -100,22 +101,17 @@ do
     sleep 1
 done
 
-# Create the WordPress database
-mysql -e "CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB};"
-
-# Create the first user
-# Create the first user
-mysql -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY '${DB_USERPASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';"
-mysql -e "CREATE USER IF NOT EXISTS 'wordpress'@'inception_backend' IDENTIFIED BY '${DB_USERPASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${WORDPRESS_DB}.* TO 'wordpress'@'inception_backend';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "DELETE FROM	mysql.user WHERE User='';"
+mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test';"
+mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '%'@'%';"
 
-# Create the second user (administrator)
-mysql -e "CREATE USER IF NOT EXISTS '${DB_USER2}'@'%' IDENTIFIED BY '${DB_USERPASS2}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${WORDPRESS_DB}.* TO '${DB_USER2}'@'%' WITH GRANT OPTION;"
+
 
 # Flush privileges
 mysql -e "FLUSH PRIVILEGES;"
