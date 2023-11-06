5b866e26f2a71a6aa982763b6ad1f4e8c989a81f
Dot-kpp <79651465+Dot-kpp@users.noreply.github.com>
2023-11-06 10:59:33 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -1,13 +1,43 @@
+# #!/bin/bash
+
+# set -e
+
+# /usr/bin/mysql --bootstrap << EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
+# DELETE FROM mysql.user WHERE User='';
+# DROP DATABASE IF EXISTS test;
+# DELETE FROM mysql.db WHERE Db='test';
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+# CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+# FLUSH PRIVILEGES;
+# EOF
+
+# exec "$@"
+
+
 #!/bin/bash
 
 set -e
 
-/usr/bin/mysql << EOF
+# Start the MariaDB server
+/usr/bin/mysqld_safe &
+
+# Wait for the MariaDB server to start
+until mysqladmin ping >/dev/null 2>&1; do
+  sleep 1
+done
+
+# Run your SQL commands
+mysql << EOF
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -15,5 +45,13 @@ CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
+
+# Stop the MariaDB server
+killall mysqld
+
+# Wait for the MariaDB server to stop
+until ! mysqladmin ping >/dev/null 2>&1; do
+  sleep 1
+done
 
 exec "$@"
