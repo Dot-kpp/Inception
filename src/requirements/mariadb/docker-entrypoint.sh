2b5e7c4f625cf8e6e631ae8a907a2c84436a991c
Dot-kpp <79651465+Dot-kpp@users.noreply.github.com>
2023-11-06 15:51:11 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -18,7 +18,7 @@ sleep 6
 #     sleep 1
 # done
 
-
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "DELETE FROM	mysql.user WHERE User='';"
