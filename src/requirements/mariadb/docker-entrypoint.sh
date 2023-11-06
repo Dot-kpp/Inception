720110b55546c7e2a41e245cd45cee55fede19e8
Dot-kpp <79651465+Dot-kpp@users.noreply.github.com>
2023-11-06 15:33:35 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -17,6 +17,7 @@ do
     sleep 1
 done
 
+
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "DELETE FROM	mysql.user WHERE User='';"
 mysql -e "DROP DATABASE IF EXISTS test;"
@@ -26,6 +27,7 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'wordpress.inception_backend' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 
 
 
