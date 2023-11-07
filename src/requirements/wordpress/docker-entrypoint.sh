85f3afab3e9ec4e1994048e1a1c2a2d3c49c95bf
Dot-kpp <79651465+Dot-kpp@users.noreply.github.com>
2023-11-07 14:21:02 -0500

    .

--- src/requirements/wordpress/docker-entrypoint.sh
+++ src/requirements/wordpress/docker-entrypoint.sh
@@ -1,7 +1,7 @@
-sleep 60
-# while ! mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &>/dev/null; do
-#     sleep 5
-# done
+sleep 10
+while ! mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &>/dev/null; do
+    sleep 5
+done
 
 while ! mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &>/dev/null; do
 	echo "for fuck sakes work bitch"
