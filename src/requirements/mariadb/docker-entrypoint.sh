51adeb58ce032aed8d15a3863385f52acc85017c
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-08 14:46:08 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -3,30 +3,9 @@ set -e
 
 # mysqld_safe --console &
 
-mysqld_safe --user=mysql &
-# Wait for the server to start
-
-while ! mysqladmin ping -hlocalhost --silent; do
-    sleep 1
-done
-
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-exec mysqld_safe
-
-
-
-#!/bin/bash
-set -e
-
-# Start the database server
 mysqld --user=mysql &
-
 # Wait for the server to start
+
 while ! mysqladmin ping -hlocalhost --silent; do
     sleep 1
 done
@@ -38,3 +17,24 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 exec mysqld_safe
+
+
+
+# #!/bin/bash
+# set -e
+
+# # Start the database server
+# mysqld --user=mysql &
+
+# # Wait for the server to start
+# while ! mysqladmin ping -hlocalhost --silent; do
+#     sleep 1
+# done
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+# mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
+# mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+# exec mysqld_safe
