cfda4eb3e626b51a1bc9004a61dcc83cdae4d056
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-08 16:32:24 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -11,7 +11,7 @@ while ! mysqladmin ping -hlocalhost --silent; do
     sleep 1
 done
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`'${MYSQL_USER}\`@'%' IDENTIFIED by '${MYSQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`'${MYSQL_USER}'\`@'%' IDENTIFIED by '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
