88559e0848832fb3ec0331f30e55e8e80c7caf64
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-08 14:32:59 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -1,36 +1,36 @@
-#!/bin/sh
+# #!/bin/sh
 
-# mysqld_safe --console &
+# # mysqld_safe --console &
 
-service mysql start;
+# service mysql start;
 
-# Wait for MariaDB server to start (max 30 seconds)
-# timeout=30
-# while ! mysqladmin ping -h localhost --silent; do
-#     timeout=$(expr $timeout - 1)
-#     if [ $timeout -eq 0 ]; then
-#         echo "Could not connect to MariaDB server. Aborting..."
-#         exit 1
-#     fi
-#     sleep 1
-# done
+# # Wait for MariaDB server to start (max 30 seconds)
+# # timeout=30
+# # while ! mysqladmin ping -h localhost --silent; do
+# #     timeout=$(expr $timeout - 1)
+# #     if [ $timeout -eq 0 ]; then
+# #         echo "Could not connect to MariaDB server. Aborting..."
+# #         exit 1
+# #     fi
+# #     sleep 1
+# # done
 
-# Wait for MariaDB server to start (max 30 seconds)
-echo "Waiting for MariaDB server to accept connections"
-sleep 2
+# # Wait for MariaDB server to start (max 30 seconds)
+# echo "Waiting for MariaDB server to accept connections"
+# sleep 2
 
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DROP DATABASE IF EXISTS test;"
-mysql -e "DELETE FROM mysql.db WHERE Db='test';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "DELETE FROM mysql.user WHERE User='';"
+# mysql -e "DROP DATABASE IF EXISTS test;"
+# mysql -e "DELETE FROM mysql.db WHERE Db='test';"
+# mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+# mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
+# mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 
-# Flush privileges
-mysql -e "FLUSH PRIVILEGES;"
+# # Flush privileges
+# mysql -e "FLUSH PRIVILEGES;"
 
 # Stop the MariaDB server
 # mysqladmin shutdown
@@ -87,3 +87,43 @@ mysql -e "FLUSH PRIVILEGES;"
 # # fi
 
 # exec "$@"
+
+
+
+#!/bin/sh
+
+# mysqld_safe --console &
+
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u rot -p"$MYSQL_ROOT_PASSWORD" shutdown
+exec mysqld_safe
+
+# # Wait for MariaDB server to start (max 30 seconds)
+# # timeout=30
+# # while ! mysqladmin ping -h localhost --silent; do
+# #     timeout=$(expr $timeout - 1)
+# #     if [ $timeout -eq 0 ]; then
+# #         echo "Could not connect to MariaDB server. Aborting..."
+# #         exit 1
+# #     fi
+# #     sleep 1
+# # done
+
+# # Wait for MariaDB server to start (max 30 seconds)
+# echo "Waiting for MariaDB server to accept connections"
+# sleep 2
+
+# mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "DELETE FROM mysql.user WHERE User='';"
+# mysql -e "DROP DATABASE IF EXISTS test;"
+# mysql -e "DELETE FROM mysql.db WHERE Db='test';"
+# mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+
+# # Flush privileges
+# mysql -e "FLUSH PRIVILEGES;"
