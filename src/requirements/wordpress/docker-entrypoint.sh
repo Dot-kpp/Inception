50e0b93ce8f4c9820eb929867fd041ea1e453317
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-08 16:18:03 -0500

    .

--- src/requirements/wordpress/docker-entrypoint.sh
+++ src/requirements/wordpress/docker-entrypoint.sh
@@ -21,7 +21,7 @@ if [ ! -f /tmp/done_config ]; then
 
 
 	wp core download --path="/var/www/html" --allow-root
-    wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" \
+  wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" \
     --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --dbcharset="utf8mb4" \
     --dbcollate="utf8mb4_general_ci" --path="/var/www/html" --allow-root || {
     echo 'Failed to create wp-config.php'
