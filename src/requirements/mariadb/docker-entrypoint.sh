fdc3a63d4c9ea52fd7c5b0346c89474ade2d88dd
Jeremie Pilotte <Jeremie.p23@gmail.com>
2023-11-08 14:27:32 -0500

    .

--- src/requirements/mariadb/docker-entrypoint.sh
+++ src/requirements/mariadb/docker-entrypoint.sh
@@ -1,87 +1,89 @@
-# #!/bin/sh
+#!/bin/sh
 
 # mysqld_safe --console &
 
-# # Wait for MariaDB server to start (max 30 seconds)
-# # timeout=30
-# # while ! mysqladmin ping -h localhost --silent; do
-# #     timeout=$(expr $timeout - 1)
-# #     if [ $timeout -eq 0 ]; then
-# #         echo "Could not connect to MariaDB server. Aborting..."
-# #         exit 1
-# #     fi
-# #     sleep 1
-# # done
+service mysql start;
 
-# # Wait for MariaDB server to start (max 30 seconds)
-# echo "Waiting for MariaDB server to accept connections"
-# sleep 2
+# Wait for MariaDB server to start (max 30 seconds)
+# timeout=30
+# while ! mysqladmin ping -h localhost --silent; do
+#     timeout=$(expr $timeout - 1)
+#     if [ $timeout -eq 0 ]; then
+#         echo "Could not connect to MariaDB server. Aborting..."
+#         exit 1
+#     fi
+#     sleep 1
+# done
 
-# mysql -e "FLUSH PRIVILEGES;"
-# mysql -e "DELETE FROM mysql.user WHERE User='';"
-# mysql -e "DROP DATABASE IF EXISTS test;"
-# mysql -e "DELETE FROM mysql.db WHERE Db='test';"
-# mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-# mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-# mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
-# mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+# Wait for MariaDB server to start (max 30 seconds)
+echo "Waiting for MariaDB server to accept connections"
+sleep 2
 
-# # Flush privileges
-# mysql -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "DELETE FROM mysql.user WHERE User='';"
+mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test';"
+mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 
-# # Stop the MariaDB server
-# # mysqladmin shutdown
+# Flush privileges
+mysql -e "FLUSH PRIVILEGES;"
 
-
-# # if [ -f ".a" ]; then
-# # 	echo "DB is already set up"
-# # else
-# # 	echo "Setting up the DB"
-# # 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
-# # 	mysqld --user=mysql --datadir=/var/lib/mysql &
-# # 	sleep 5
-
-# # 	mysql -e "create database if not exists ${MYSQL_DATABASE};"
-# # 	echo "DB is created"
-
-# # 	mysql -e "create user if not exists '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';"
-# # 	echo "User ${MYSQL_USER} created"
-
-# # 	mysql -e "grant all privileges on \`${MYSQL_DATABASE}\`.* to \`${MYSQL_USER}\`@'%' identified by '${MYSQL_PASSWORD}';"
-# # 	echo "Privileges have been granted"
-
-# # 	mysql -e "alter user 'root'@'localhost' identified by '${MYSQL_ROOT_PASSWORD}';"
-# # 	echo "Root password is now set"
-
-# # 	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "flush privileges;"
-# # 	echo "Privileges have been flushed"
-
-# # 	touch ".a"
-# # 	pkill mysqld
-
-# # fi
+# Stop the MariaDB server
+# mysqladmin shutdown
 
 
-#!/bin/bash
+# if [ -f ".a" ]; then
+# 	echo "DB is already set up"
+# else
+# 	echo "Setting up the DB"
+# 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-set -e
+# 	mysqld --user=mysql --datadir=/var/lib/mysql &
+# 	sleep 5
 
-/usr/sbin/mysqld --bootstrap << EOF
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
+# 	mysql -e "create database if not exists ${MYSQL_DATABASE};"
+# 	echo "DB is created"
+
+# 	mysql -e "create user if not exists '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';"
+# 	echo "User ${MYSQL_USER} created"
+
+# 	mysql -e "grant all privileges on \`${MYSQL_DATABASE}\`.* to \`${MYSQL_USER}\`@'%' identified by '${MYSQL_PASSWORD}';"
+# 	echo "Privileges have been granted"
+
+# 	mysql -e "alter user 'root'@'localhost' identified by '${MYSQL_ROOT_PASSWORD}';"
+# 	echo "Root password is now set"
+
+# 	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "flush privileges;"
+# 	echo "Privileges have been flushed"
+
+# 	touch ".a"
+# 	pkill mysqld
 
 # fi
 
-exec "$@"
+
+# #!/bin/bash
+
+# set -e
+
+# /usr/sbin/mysqld --bootstrap << EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
+# DELETE FROM	mysql.user WHERE User='';
+# DROP DATABASE IF EXISTS test;
+# DELETE FROM mysql.db WHERE Db='test';
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+# CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+# FLUSH PRIVILEGES;
+# EOF
+
+# # fi
+
+# exec "$@"
